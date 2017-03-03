42639164ccb163e3359c17023d25a93ff67e07dc
aajju <aajju@naver.com>
2017-03-03 13:53:06 +0900

    login 테스트 구현 완료

--- app/src/main/java/com/aajju/memoserverexam/JoinActivity.java
+++ app/src/main/java/com/aajju/memoserverexam/JoinActivity.java
@@ -1,13 +1,86 @@
 package com.aajju.memoserverexam;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class JoinActivity extends AppCompatActivity {
+
+    private EditText mEmail, mPassword;
+    private int mCheck = -1;
+    private Api mApi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        mEmail = (EditText) findViewById(R.id.join_email_et);
+        mPassword = (EditText) findViewById(R.id.join_password_et);
+
+        mApi = HttpHelper.getAPI();
+
+        // 중복체크 버튼
+        findViewById(R.id.check_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(TextUtils.isEmpty(mEmail.getText().toString())){
+                    Toast.makeText(JoinActivity.this, "이메일을 입력해주세요", Toast.LENGTH_SHORT).show();
+                } else {
+                    mApi.duplicateCheck(mEmail.getText().toString()).enqueue(new Callback<Void>() {
+                        @Override
+                        public void onResponse(Call<Void> call, Response<Void> response) {
+                            if(response.code() == 200){  // 중복 아님
+                                mCheck = 1;
+                                Toast.makeText(JoinActivity.this, "사용 가능한 이메일 주소입니다", Toast.LENGTH_SHORT).show();
+                            } else if (response.code() == 409){   // 중복
+                                Toast.makeText(JoinActivity.this, "이미 존재하는 이메일 주소입니다. \n 다른 이메일을 입력해주세요", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<Void> call, Throwable t) {
+                            Toast.makeText(JoinActivity.this, "onFailure", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+                }
+
+            }
+        });
+
+        // 회원 가입 완료 버튼
+        findViewById(R.id.join_ok_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(TextUtils.isEmpty(mEmail.getText().toString()) || TextUtils.isEmpty(mPassword.getText().toString())){
+                    Toast.makeText(JoinActivity.this, "둘다 입력하세요", Toast.LENGTH_SHORT).show();
+                } else {
+                    mApi.join(mEmail.getText().toString(), mPassword.getText().toString()).enqueue(new Callback<Void>() {
+                        @Override
+                        public void onResponse(Call<Void> call, Response<Void> response) {
+                            setResult(RESULT_OK);
+                            finish();
+                        }
+
+                        @Override
+                        public void onFailure(Call<Void> call, Throwable t) {
+
+                        }
+                    });
+                }
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/aajju/memoserverexam/MainActivity.java
+++ app/src/main/java/com/aajju/memoserverexam/MainActivity.java
@@ -1,7 +1,10 @@
 package com.aajju.memoserverexam;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
@@ -13,31 +16,36 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     private Api mApi;
+    private ProgressDialog mProgressDialog;
+    private EditText mEmail, mPassword;
+    private String mToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText email = (EditText) findViewById(R.id.email_et);
-        final EditText password = (EditText) findViewById(R.id.password_et);
+        mEmail = (EditText) findViewById(R.id.email_et);
+        mPassword = (EditText) findViewById(R.id.password_et);
 
         mApi = HttpHelper.getAPI();
 
         findViewById(R.id.login_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(TextUtils.isEmpty(email.getText().toString()) || TextUtils.isEmpty(password.getText().toString())){
+                if(TextUtils.isEmpty(mEmail.getText().toString()) || TextUtils.isEmpty(mPassword.getText().toString())){
                     Toast.makeText(MainActivity.this, "둘다 입력하세요", Toast.LENGTH_SHORT).show();
                 } else {
-                    mApi.login(email.getText().toString(), password.getText().toString()).enqueue(new Callback<Void>() {
+                    mApi.login(mEmail.getText().toString(), mPassword.getText().toString()).enqueue(new Callback<Void>() {
                         @Override
                         public void onResponse(Call<Void> call, Response<Void> response) {
                             if(!response.isSuccessful()){
-                                Toast.makeText(MainActivity.this, "회원 정보가 없습니다", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(MainActivity.this, "아이디와 비밀번호에 해당하는 회원 정보가 없습니다. \n 다시 입력해주세요", Toast.LENGTH_SHORT).show();
                                 return;
                             }
-                            Toast.makeText(MainActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+                            mToken = response.headers().get("x-auth-token");
+                            new TestAsyncTask().execute();
+//                            Toast.makeText(MainActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
                         }
 
                         @Override
@@ -48,5 +56,53 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        findViewById(R.id.join_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(MainActivity.this, JoinActivity.class), 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 100 && resultCode == RESULT_OK){
+            Toast.makeText(this, "회원 가입 성공", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class TestAsyncTask extends AsyncTask<Void, String, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(MainActivity.this);
+            mProgressDialog.setCancelable(false);
+            mProgressDialog.setMessage("로그인 시도 중!");
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            mProgressDialog.dismiss();
+            Intent intent = new Intent(MainActivity.this, ListActivity.class);
+            intent.putExtra("tokem", mToken);
+
+            startActivity(intent);
+        }
     }
 }
