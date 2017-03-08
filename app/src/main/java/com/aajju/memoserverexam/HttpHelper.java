609f1a34afca11a46967ff7b56b7bb85b139016e
prChoe <massivcode@gmail.com>
2017-03-08 13:12:00 +0900

    로그용 라이브러리 추가

--- app/src/main/java/com/aajju/memoserverexam/HttpHelper.java
+++ app/src/main/java/com/aajju/memoserverexam/HttpHelper.java
@@ -1,6 +1,7 @@
 package com.aajju.memoserverexam;
 
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -25,8 +26,10 @@ public class HttpHelper {
     }
 
     private static OkHttpClient getClient() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
         return new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
                 .build();
-
     }
 }
