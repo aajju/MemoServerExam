626aa29d337953fc2db416578b4f2d1f933c021e
prChoe <massivcode@gmail.com>
2017-03-08 13:46:18 +0900

    로그인 화면 다이얼로그 관련 수정

--- app/src/main/java/com/aajju/memoserverexam/MainActivity.java
+++ app/src/main/java/com/aajju/memoserverexam/MainActivity.java
@@ -2,7 +2,6 @@ package com.aajju.memoserverexam;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -33,27 +32,33 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.login_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(TextUtils.isEmpty(mEmail.getText().toString()) || TextUtils.isEmpty(mPassword.getText().toString())){
+                if (TextUtils.isEmpty(mEmail.getText().toString()) || TextUtils.isEmpty(mPassword.getText().toString())) {
                     Toast.makeText(MainActivity.this, "둘다 입력하세요", Toast.LENGTH_SHORT).show();
-                } else {
-                    mApi.login(mEmail.getText().toString(), mPassword.getText().toString()).enqueue(new Callback<Void>() {
-                        @Override
-                        public void onResponse(Call<Void> call, Response<Void> response) {
-                            if(!response.isSuccessful()){
-                                Toast.makeText(MainActivity.this, "아이디와 비밀번호에 해당하는 회원 정보가 없습니다. \n 다시 입력해주세요", Toast.LENGTH_SHORT).show();
-                                return;
-                            }
-                            mToken = response.headers().get("x-auth-token");
-//                            Toast.makeText(MainActivity.this, mToken, Toast.LENGTH_SHORT).show();
-                            new TestAsyncTask().execute();
+                    return;
+                }
+
+
+                showProgressDialog();
+
+                mApi.login(mEmail.getText().toString(), mPassword.getText().toString()).enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        dismissDialog();
+                        if (!response.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "아이디와 비밀번호에 해당하는 회원 정보가 없습니다. \n 다시 입력해주세요", Toast.LENGTH_SHORT).show();
+                            return;
                         }
 
-                        @Override
-                        public void onFailure(Call<Void> call, Throwable t) {
-                            Toast.makeText(MainActivity.this, "서버에서 데이터 가져오기 실패", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
+                        mToken = response.headers().get("x-auth-token");
+                        startActivity(new Intent(MainActivity.this, ListActivity.class).putExtra("token", mToken));
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        dismissDialog();
+                        Toast.makeText(MainActivity.this, "서버에서 데이터 가져오기 실패", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
@@ -65,44 +70,30 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(MainActivity.this);
+            mProgressDialog.setCancelable(false);
+            mProgressDialog.setMessage("로그인 시도중...");
+        }
+
+        mProgressDialog.show();
+    }
+
+    private void dismissDialog() {
+        if (mProgressDialog != null) {
+            mProgressDialog.dismiss();
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == 100 && resultCode == RESULT_OK){
+        if (requestCode == 100 && resultCode == RESULT_OK) {
             Toast.makeText(this, "회원 가입 성공", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private class TestAsyncTask extends AsyncTask<Void, String, Void> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            mProgressDialog = new ProgressDialog(MainActivity.this);
-            mProgressDialog.setCancelable(false);
-            mProgressDialog.setMessage("로그인 시도 중!");
-            mProgressDialog.show();
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            mProgressDialog.dismiss();
-            Intent intent = new Intent(MainActivity.this, ListActivity.class);
-            intent.putExtra("token", mToken);
-
-            startActivity(intent);
-        }
-    }
 }
